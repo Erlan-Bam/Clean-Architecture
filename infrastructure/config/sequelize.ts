7131d891cbe25121e74624876ffe7dac208fa4db
Erlan-bam <erlanzh.gg@gmail.com>
2024-05-30 15:08:17 +0500

    first commit

---
+++ infrastructure/config/sequelize.ts
@@ -0,0 +1,26 @@
+import { Sequelize } from "sequelize-typescript";
+
+// Подключение к базе данных
+const sequelize = new Sequelize({
+    repositoryMode: true,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: 5432,
+    dialect: "postgres",
+    storage: ":memory:",
+    logging: false,
+    models: [],
+});
+
+export default sequelize;
+
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("Database synchronized");
+  })
+  .catch((error) => {
+    console.error("Failed to synchronize database:", error);
+  });
